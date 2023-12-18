efa28a23dc4b44ab4baddfed477cfe8a687a903d
EvilKanoa <kanoa@kanoa.ca>
2023-12-18 00:49:51 -0500

    chore: done day 18

--- shared/math.ts
+++ shared/math.ts
@@ -2,6 +2,22 @@ import _pointInPolygon from "robust-point-in-polygon";
 
 export const pointInPolygon = _pointInPolygon;
 
+export const polygonArea = (vertices: [number, number][]) => {
+  let total = 0;
+
+  for (let i = 0, l = vertices.length; i < l; i++) {
+    const addX = vertices[i][0];
+    const addY = vertices[i == vertices.length - 1 ? 0 : i + 1][1];
+    const subX = vertices[i == vertices.length - 1 ? 0 : i + 1][0];
+    const subY = vertices[i][1];
+
+    total += addX * addY * 0.5;
+    total -= subX * subY * 0.5;
+  }
+
+  return Math.abs(total);
+};
+
 export const gcd = (a: number, b: number): number => (a ? gcd(b % a, a) : b);
 
 export const lcm = (a: number, b: number): number => (a * b) / gcd(a, b);
