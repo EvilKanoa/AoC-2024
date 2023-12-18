1753a9f6c91360c9499d1f7c7bb0627d722bd887
EvilKanoa <kanoa@kanoa.ca>
2023-12-18 17:55:12 -0500

    chore: add stats capture to memoize util

--- shared/memo.ts
+++ shared/memo.ts
@@ -1,5 +1,6 @@
 export const MEMO_KEY_CACHE = Symbol.for("memoCacheKeySymbol");
 export const MEMO_KEY_UPDATE = Symbol.for("memoUpdateKeySymbol");
+export const MEMO_KEY_STATS = Symbol.for("memoStatsKeySymbol");
 
 type MemoFunc<Arguments extends unknown[], Result> = (
   ...args: Arguments
@@ -10,6 +11,7 @@ type UpdateFunc<Result> = (source: Map<string, Result>) => void;
 interface MemoizedExtra<Result> {
   [MEMO_KEY_CACHE]: Map<string, Result>;
   [MEMO_KEY_UPDATE]: UpdateFunc<Result>;
+  [MEMO_KEY_STATS]: { hits: number; misses: number };
 }
 
 export type Memoized<Arguments extends unknown[], Result> = MemoFunc<
@@ -18,21 +20,53 @@ export type Memoized<Arguments extends unknown[], Result> = MemoFunc<
 > &
   MemoizedExtra<Result>;
 
+/**
+ * Returns a memoized version of the given function. Memoization
+ * occurs through `JSON.stringify` on the arguments. All arguments
+ * must be JSON serializable. The memoized version will have the
+ * properties listed in `MemoizedExtra` available to either access
+ * the result cache or update the result cache.
+ *
+ * Optionally, you can enable stats for the memoized function that
+ * will record total hits and misses to the function. By default,
+ * this is disabled for performance. You cannot modify this setting
+ * after creating the memoized function.
+ * @param func The function to memoize
+ * @param captureStats Flag to enable stats capture
+ * @returns The memoized version of func
+ */
 export const memoize = <Arguments extends unknown[], Result>(
-  func: MemoFunc<Arguments, Result>
+  func: MemoFunc<Arguments, Result>,
+  captureStats = false
 ): Memoized<Arguments, Result> => {
   const cache = new Map<string, Result>();
+  const stats = { hits: 0, misses: 0 };
 
-  const memoized = (...args: Arguments) => {
-    const key = JSON.stringify(args);
-    if (key in cache) {
-      return cache.get(key)!;
-    }
-    const result = func(...args);
-    cache.set(key, result);
-    return result;
-  };
+  const memoized = (
+    captureStats
+      ? (...args: Arguments) => {
+          const key = JSON.stringify(args);
+          if (key in cache) {
+            stats.hits++;
+            return cache.get(key)!;
+          }
+          const result = func(...args);
+          cache.set(key, result);
+          stats.misses++;
+          return result;
+        }
+      : (...args: Arguments) => {
+          const key = JSON.stringify(args);
+          if (key in cache) {
+            return cache.get(key)!;
+          }
+          const result = func(...args);
+          cache.set(key, result);
+          return result;
+        }
+  ) as Memoized<Arguments, Result>;
 
+  memoized[MEMO_KEY_STATS] = stats;
   memoized[MEMO_KEY_CACHE] = cache;
   memoized[MEMO_KEY_UPDATE] = ((source) => {
     for (const [key, result] of source) {
