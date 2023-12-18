af7bace8e99ceb84e34db88cae3c0f3dfc7929e7
EvilKanoa <kanoa@kanoa.ca>
2023-12-18 16:00:58 -0500

    chore: day 15 part a done

---
+++ days/15/src/solve.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, it } from "bun:test";
+import { partA, partB } from "./solve";
+
+const exampleInput = `
+rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+`;
+const expectedOutputA = 1320;
+const expectedOutputB = 0;
+
+describe("part A", () => {
+  it("should pass", async () => {
+    expect(await partA(exampleInput.trim().split("\n"))).toEqual(
+      expectedOutputA
+    );
+  });
+});
+
+describe("part B", () => {
+  it("should pass", async () => {
+    expect(await partB(exampleInput.trim().split("\n"))).toEqual(
+      expectedOutputB
+    );
+  });
+});
