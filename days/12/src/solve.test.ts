faff0adc38bda352d79332f59181b8c05fbbe877
EvilKanoa <kanoa@kanoa.ca>
2024-12-22 16:07:46 -0500

    chore: first go at day 12

---
+++ days/12/src/solve.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from "bun:test";
+import { partA, partB } from "./solve";
+
+const exampleInput = `
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+`;
+const expectedOutputA = 1930;
+const expectedOutputB = 0;
+
+describe("part A", () => {
+  it("should pass", async () => {
+    expect(await partA(exampleInput.trim().split("\n"))).toEqual(
+      expectedOutputA
+    );
+  });
+});
+
+describe("part B", () => {
+  it("should pass", async () => {
+    expect(await partB(exampleInput.trim().split("\n"))).toEqual(
+      expectedOutputB
+    );
+  });
+});
--- shared/math.ts
+++ shared/math.ts
@@ -2,20 +2,50 @@ import _pointInPolygon from "robust-point-in-polygon";
 
 export const pointInPolygon = _pointInPolygon;
 
-export const polygonArea = (vertices: [number, number][]) => {
+export type Polygon =
+  | [number, number][]
+  | { vertices: [number, number][]; holes: [number, number][][] };
+
+export const polygonArea = (polygon: Polygon): number => {
+  const vertices = Array.isArray(polygon) ? polygon : polygon.vertices;
+
   let total = 0;
 
   for (let i = 0, l = vertices.length; i < l; i++) {
     const addX = vertices[i][0];
-    const addY = vertices[i == vertices.length - 1 ? 0 : i + 1][1];
-    const subX = vertices[i == vertices.length - 1 ? 0 : i + 1][0];
+    const addY = vertices[i === vertices.length - 1 ? 0 : i + 1][1];
+    const subX = vertices[i === vertices.length - 1 ? 0 : i + 1][0];
     const subY = vertices[i][1];
 
     total += addX * addY * 0.5;
     total -= subX * subY * 0.5;
   }
 
-  return Math.abs(total);
+  total = Math.abs(total);
+
+  if (!Array.isArray(polygon)) {
+    total -= polygon.holes.map(polygonArea).reduce(sum);
+  }
+
+  return total;
+};
+
+export const polygonPerimeter = (polygon: Polygon): number => {
+  const vertices = Array.isArray(polygon) ? polygon : polygon.vertices;
+
+  let total = 0;
+
+  for (let i = 1, l = vertices.length; i < l; i++) {
+    const dx = Math.abs(vertices[i][0] - vertices[i - 1][0]);
+    const dy = Math.abs(vertices[i][1] - vertices[i - 1][1]);
+    total += Math.sqrt(dx * dx + dy * dy);
+  }
+
+  if (!Array.isArray(polygon)) {
+    total += polygon.holes.map(polygonPerimeter).reduce(sum);
+  }
+
+  return total;
 };
 
 export const gcd = (a: number, b: number): number => (a ? gcd(b % a, a) : b);
